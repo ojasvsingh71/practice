c675a081c73e31faaf6f3802a10281b5a437c528
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-04-18 18:37:50 +0530

    used bcrypt to hash password

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,16 +1,18 @@
 import buu from "../models/bu.model.js"
+import bcrypt from "bcrypt"
 
 const register = async (req, res) => {
     const { username, password, age, gender } = req.body;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await buu.create({
         username: username,
-        password: password,
+        password: hashedPassword,
         age: age,
         gender: gender
     })
 
-    res.status(201).json({
+    res.status(200).json({
         message: "Registered Bu...",
         user
     })
@@ -20,12 +22,13 @@ const login = async (req, res) => {
     const { username, password, gender } = req.body;
 
     const bu = await buu.findOne({ username });
+    const isPassMatch = await bcrypt.compare(password, bu.password);
 
     if (!bu) {
         res.status(404).json({
             message: "Cannot find bu :("
         })
-    } else if (password !== bu.password || gender !== bu.gender) {
+    } else if (!isPassMatch || gender !== bu.gender) {
         res.status(401).json({
             message: "Incorrect password :( or :]"
         })
