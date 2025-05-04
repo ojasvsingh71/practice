39fcf9993c2d363a135679bb011407437cd9bbb1
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-05-04 15:48:11 +0530

    frontend recap

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Profile from "./components/profile_pic";
+
+function App() {
+  return (
+    <div>
+      <h1>OJASV</h1>
+      <Profile />
+
+    </div>
+  )
+
+}
+
+export default App;
---
+++ client/src/components/profile_pic.js
@@ -0,0 +1,7 @@
+export default function Profile() {
+    return (
+        <div>
+            <img src="https://cdn.noitatnemucod.net/thumbnail/1366x768/100/e5dee087051734a86d59c748b6bf201a.jpg" alt="bu" />
+        </div>
+    )
+}
