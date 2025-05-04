e552b61923b9b2104094ef5c37ee331d1502bfaf
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-05-04 16:58:08 +0530

    just a card

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import Profile from "./components/profile_pic";
 
+const ojasv = {
+  name: "Ojasv Singh",
+  age: 18,
+  pic: "https://hianimez.to/images/discussion.png",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+    display: "inline-block"
+  }
+}
+
 function App() {
   return (
-    <div>
-      <h1>OJASV</h1>
-      <Profile />
+    <div style={ojasv.theme}>
+      <h1>{ojasv.name}</h1>
+      <img
+        src={ojasv.pic}
+        alt={ojasv.name}
+      />
 
-    </div>
+    </div >
   )
 
 }
